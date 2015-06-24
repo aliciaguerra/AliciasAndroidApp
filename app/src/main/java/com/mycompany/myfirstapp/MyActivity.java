afa46f26361db8593905aeff888818c8f8bfe7f3
aliciaguerra <aiguerra@ucsd.edu>
2015-06-24 10:39:15 -0700

    Initial commit

---
+++ app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -0,0 +1,45 @@
+package com.mycompany.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+
+
+
+
+public class MyActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        //shows icon in actionbar
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    /** Called when the user clicks the Send button */
+    public void about(View view) {
+        Intent intent = new Intent(this, AboutAlicia.class);
+        startActivity(intent);
+    }
+
+}
